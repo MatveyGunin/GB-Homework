4653d4607e5e1b2295e41f2aa9d31ab1390173ee
Matvey Gunin <45951847+MatveyGunin@users.noreply.github.com>
2021-06-03 00:58:01 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        
+            string name = Console.ReadLine();
+        
+            Console.WriteLine("Привет, " + name + ", сегодня " +  DateTime.Now);
+        }
+    }
+}
