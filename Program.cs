fd5369effab6d903f7b1e498fd8ffb3e4dea5a34
Matvey Gunin <45951847+MatveyGunin@users.noreply.github.com>
2021-06-09 23:43:37 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,147 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Globalization;
-using System.Linq;
-using System.Reflection;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace ConsoleApp3
 {
     class Program
     {
-        
-
         static void Main(string[] args)
-
         {
-            string NameOfStore;
-            int a = 20, b = 35, c = 45, NameOfProduct, aa;
-
-            Console.WriteLine("Введите название магазина");
-            NameOfStore = Console.ReadLine();
-
-            
-            
-            Console.WriteLine("Выберите товар, который хотите купить");
-            Console.WriteLine("1. Тетрадь школьная 18 листов - " + a + "рублей");
-            Console.WriteLine("2. Тетрадь школьная 24 листа - " + b + "рублей");
-            Console.WriteLine("3. Тетрадь школьная 36 листов - " + c + "рублей");
-            NameOfProduct = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Введите количество покупаемого товара");
-            aa = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("ООО " + NameOfStore);
-            Console.WriteLine(DateTime.Now);
-
-
-
-            if (NameOfProduct == 1) 
-                {
-                    
-                    Console.WriteLine("Тетрадь школьная 18 листов - " + aa + " штук " + a * aa + " руб.");
-               
-
-
-                }
-               if (NameOfProduct == 2)
-            {
-                   
-                    Console.WriteLine("Тетрадь школьная 24 листа - " + aa + " штук " + b * aa + " руб.");
-
-                }
-                if (NameOfProduct == 3) 
-                {
-                   
-                    Console.WriteLine("Тетрадь школьная 36 листов - " + aa + " штук " + c * aa + " руб.");
-
-                }
-            
-
-
-
-
-
-
-
-
-
-
-
-            }
-
-
-
-
-
-
-
-
- }
+            int MaxValue, MinValue;
+            Console.WriteLine("Введите максимальную температуру за месяц");
+            MaxValue = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите минимальную температуру за месяц");
+            MinValue = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Средняя температура за месяц - " + (MaxValue + MinValue) / 2);
+        }
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-   
-            
-            
-           
-
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        
-
+}
